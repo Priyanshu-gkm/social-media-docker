3f963652520766089d8bfe0b3e3b3cf0899d0b32
Priyanshu Arora <priyanshu@gkmit.co>
2023-11-23 16:46:22 +0530

    add debug from environment

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from social_media_application import create_app
+import os
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=eval(os.environ.get("DEBUG")))
